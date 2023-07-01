47ae1c2a07c334627401cfd6afd29a17b0ee1bd6
nancy <smithnancy@outlook.com>
2023-07-01 23:15:20 +1000

    spinner appears when get quote stars and is removed when the results are ready

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, onMounted, reactive, computed } from 'vue'
   import Alert from './components/Alert.vue'
+  import Spinner from './components/Spinner.vue'
 
   const coins = ref([
         { code: 'USD', text: 'USD'},
@@ -19,6 +20,8 @@
   const price = ref({})
 
   const error = ref('')
+
+  const loading = ref(false)
 
   onMounted(() => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
@@ -40,13 +43,20 @@
   }
 
   const getQuote = async () => {
-    const { coin, criptoCoin } = quote
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoCoin}&tsyms=${coin}`
-    //console.log(url)
-    const response = await fetch(url)
-    const data = await response.json()
-    //console.log(data.DISPLAY[criptoCoin][coin])
-    price.value = data.DISPLAY[criptoCoin][coin]
+    loading.value = true
+    price.value == {}
+    try {
+      const { coin, criptoCoin } = quote
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoCoin}&tsyms=${coin}`
+      //console.log(url)
+      const response = await fetch(url)
+      const data = await response.json()
+      //console.log(data.DISPLAY[criptoCoin][coin])
+      price.value = data.DISPLAY[criptoCoin][coin]
+      loading.value = false
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const displayResult = computed(() => {
@@ -96,6 +106,7 @@
 
         <input type="submit" value="">
       </form>
+      <Spinner v-if="loading"/>
       <div v-if="displayResult" class="contenedor-resultado">
         <h2> Quote Price</h2>
         <div class="resultado">
